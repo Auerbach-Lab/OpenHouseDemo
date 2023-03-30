1977b58754f432bfb5a2d21a6e6cc85145e59544
Noelle James <github@nohelix.com>
2023-03-30 10:44:57 -0500

    Converted to function for calling in shiney app.

--- Grapher.R
+++ Grapher.R
@@ -1,65 +1,67 @@
-# Adjust player data ------------------------------------------------------
-player_data =
-  scores %>%
-  # artificially assign an intensity value
-  rename(`40` = quiet, `60` = mid, `90` = loud) %>%
-  # convert to long format
-  tidyr::gather(key = "intensity", value = "reaction", num_range("", 10:90)) %>%
-  mutate(intensity = as.numeric(intensity))
+Grapher <- function(scores) {
+ # Adjust player data ------------------------------------------------------
+  player_data =
+    scores %>%
+    # artificially assign an intensity value
+    rename(`40` = quiet, `60` = mid, `90` = loud) %>%
+    # convert to long format
+    tidyr::gather(key = "intensity", value = "reaction", num_range("", 10:90)) %>%
+    mutate(intensity = as.numeric(intensity))
 
 
-# Rat Graph ---------------------------------------------------------------
-# Make 1st if wanted underneath
+  # Rat Graph ---------------------------------------------------------------
+  # Make 1st if wanted underneath
 
-# Make Human graph --------------------------------------------------------
-Human_graph =
-  player_data %>%
-  # may need to convert from low, medium, high to number using mutate
-  ggplot(aes(x = intensity, y = reaction, color = as.factor(player), group = as.factor(player))) +
-  stat_summary(fun = mean, geom = "line", linewidth = 2) +
-  stat_summary(fun = mean, geom = "point", size = 5)
+  # Make Human graph --------------------------------------------------------
+  Human_graph =
+    player_data %>%
+    # may need to convert from low, medium, high to number using mutate
+    ggplot(aes(x = intensity, y = reaction, color = as.factor(player), group = as.factor(player))) +
+    stat_summary(fun = mean, geom = "line", linewidth = 2) +
+    stat_summary(fun = mean, geom = "point", size = 5)
 
-# print(Human_graph)
-
-
-# Add rat data ------------------------------------------------------------
-Final_graph =
-  Human_graph +
-    # Rat data
-    geom_smooth(data = rat_data,
-                se = FALSE, na.rm = TRUE, linewidth = 2, linetype = "dotdash",
-                method = "lm", formula = y ~ x
-    ) +
-    geom_smooth(data = Human_data,
-                se = FALSE, na.rm = TRUE, linewidth = 2, linetype = "dotdash",
-                method = "lm", formula = y ~ x
-    ) +
-  scale_color_manual(values = c(
-    "1" = "mediumorchid", "2" = "grey99", "3" = "seagreen3", "0" = "royalblue", "Rat" = "firebrick3", "Human" = "gold"
-  )) +
-  labs(x = "Loudness\n(Intensity, dB)",
-       y = "Speed\n(Average reaction time, ms)",
-       color = "Player") +
-  scale_x_continuous(breaks = seq(-50, 90, by = 10)) +
-  labs(title = "<span style='color:#CD2626;'>Rat</span> v. <span style='color:#FFD700;'>Human</span>: *the ultimate hearing test*") +
-  annotate(geom = "text", x = 38, y = c(129, max(player_data$reaction, rat_data$reaction)),
-           label = c("Fast", "Slow" ), size = 8) +
-  annotate(geom = "text", x = c(42,88), y = 92,
-           label = c("Quiet", "Loud" ), size = 8) +
-  theme_ft_rc() +
-  theme(
-    # plot.title = element_text(size = 32),
-    plot.title = element_markdown(size = 32)
-  )
+  # print(Human_graph)
 
 
-print(Final_graph)
+  # Add rat data ------------------------------------------------------------
+  Final_graph =
+    Human_graph +
+      # Rat data
+      geom_smooth(data = rat_data,
+                  se = FALSE, na.rm = TRUE, linewidth = 2, linetype = "dotdash",
+                  method = "lm", formula = y ~ x
+      ) +
+      geom_smooth(data = Human_data,
+                  se = FALSE, na.rm = TRUE, linewidth = 2, linetype = "dotdash",
+                  method = "lm", formula = y ~ x
+      ) +
+    scale_color_manual(values = c(
+      "1" = "mediumorchid", "2" = "grey99", "3" = "seagreen3", "0" = "royalblue", "Rat" = "firebrick3", "Human" = "gold"
+    )) +
+    labs(x = "Loudness\n(Intensity, dB)",
+         y = "Speed\n(Average reaction time, ms)",
+         color = "Player") +
+    scale_x_continuous(breaks = seq(-50, 90, by = 10)) +
+    labs(title = "<span style='color:#CD2626;'>Rat</span> v. <span style='color:#FFD700;'>Human</span>: *the ultimate hearing test*") +
+    annotate(geom = "text", x = 38, y = c(129, max(player_data$reaction, rat_data$reaction)),
+             label = c("Fast", "Slow" ), size = 8) +
+    annotate(geom = "text", x = c(42,88), y = 92,
+             label = c("Quiet", "Loud" ), size = 8) +
+    theme_ft_rc() +
+    theme(
+      # plot.title = element_text(size = 32),
+      plot.title = element_markdown(size = 32)
+    )
 
 
-# Keep running total of human data ----------------------------------------
+  print(Final_graph)
 
-Human_data = bind_rows(Human_data, player_data %>%
-                                          mutate(player = "Human")
-                       )
 
-data.table::fwrite(Human_data, file = "human_average.csv")
+  # Keep running total of human data ----------------------------------------
+
+  Human_data = bind_rows(Human_data, player_data %>%
+                                            mutate(player = "Human")
+                         )
+
+  data.table::fwrite(Human_data, file = "human_average.csv")
+}
